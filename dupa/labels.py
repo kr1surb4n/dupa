ee322f36c8a7639f9c59ceb4553355500f9b1540
Kris Urbanski <przemek.kris.urbanski@gmail.com>
2021-04-16 10:36:24 +0200

    Cleaned up the labels

--- dupa/labels.py
+++ dupa/labels.py
@@ -1,12 +1,23 @@
 """Labels
 A set of decorators that indicate if a function
 has an unsolved bug in some case, might be not working,
-or is not tested, drafted"""
+or is not tested, maybe drafted.
+
+
+If something is faulty, the person who knows it,
+should mark this part of code accordingly, so that
+that other person would know what to expect.
+
+"""
 
 from functools import wraps
 import logging as log
-import warnings
 
+
+DEPRECATED_MESSAGE = " DEPRECATED: Deprecated. Do not use"
+DEPRECATING_MESSAGE = " DEPRECATING: Will be deprecated in the future"
+FELER_MESSAGE = " FELER: Not working partially or enteirly"
+NOT_TESTED_MESSAGE = " NOT TESTED: Function not tested"
 
 def make_label_decorator(label):        
     def label_decorator(func):
@@ -18,18 +29,26 @@ def make_label_decorator(label):
     return label_decorator
 
 
+"""
+Regular labels that should be used to mark whole files
+"""
+
 def label_as_deprecated():
-    log.warning(" DEPRECATED: Deprecated. Do not use")
+    log.warning(DEPRECATED_MESSAGE)
 
 def label_as_deprecating():
-    log.warning(" DEPRECATING: Will be deprecated in the future")
+    log.warning(DEPRECATING_MESSAGE)
 
 def label_as_feler():
-    log.warning(" FELER: Not working partially or enteirly")
+    log.warning(FELER_MESSAGE)
 
 def label_as_not_tested():
-    log.warning(" NOT TESTED: Function not tested")
+    log.warning(NOT_TESTED_MESSAGE)
         
+
+"""
+Decorators that should mark functions and classes
+"""
 
 deprecated = make_label_decorator(label_as_deprecated)
 deprecating = make_label_decorator(label_as_deprecating)
